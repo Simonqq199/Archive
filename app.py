a429afd6b035ee9fb743c890977495b9181b2cec
Simonqq199 <88841451+Simonqq199@users.noreply.github.com>
2023-06-18 20:06:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
-import os  
-from flask import Flask, request, Response, render_template, send_from_directory
-  
-app = Flask(__name__)  
-app.config['UPLOAD_FOLDER'] = 'uploads'  
-  
-@app.route('/')  
-def home():  
-    return 'Hello, World!'  
-  
-@app.route('/upload', methods=['POST'])  
-def upload_file():  
-    file = request.files['file']  
+from flask import Flask, request, Response, render_template, send_file, send_from_directory, after_this_request
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
     # Save the file to the uploads folder
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))  
-    return 'File uploaded successfully'  
-  
-@app.route('/download', methods=['GET', 'POST'])
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return 'File uploaded successfully'
+
+@app.route('/download')
 def download_file():
-    if request.method == 'POST':
-        filename = request.form['filename']
-        directory = app.config['UPLOAD_FOLDER']
-        return send_from_directory(directory, filename, as_attachment=True)
-    return render_template('download.html')
-  
-@app.route('/upload-form')  
-def upload_form():  
+    filename = 'example.pdf'
+    file_path = app.config['UPLOAD_FOLDER'] + "/" + filename
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(file_path)
+        except Exception as error:
+            app.logger.error("Error removing downloaded file", error)
+        return response
+    response = send_file(file_path, as_attachment=True)
+    response.headers["Content-Disposition"] = "attachment; filename=" + filename
+    return response
+
+@app.route('/upload-form')
+def upload_form():
     return render_template('upload.html')
-  
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     app.run(debug=True)
