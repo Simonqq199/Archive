6cf3cb6ec05fd85b26c54dea4423f2013417b06e
Simonqq199 <88841451+Simonqq199@users.noreply.github.com>
2023-06-18 19:36:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os  
-from flask import Flask, request, Response, render_template  
+from flask import Flask, request, Response, render_template, send_from_directory
   
 app = Flask(__name__)  
 app.config['UPLOAD_FOLDER'] = 'uploads'  
@@ -15,37 +15,17 @@ def upload_file():
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))  
     return 'File uploaded successfully'  
   
-@app.route('/download', methods=['GET', 'POST'])  
-def download_file():  
-    if request.method == 'POST':  
-        filename = request.form['filename']  
-        # Set the file path and name
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)  
-        # Check if the file exists
-        if not os.path.exists(path):  
-            return "File not found"  
-        # Open the file and send it as a response
-        with open(path, 'rb') as f:  
-            file = f.read()  
-        # Set the Content-Disposition header with the filename of the uploaded file
-        headers = {'Content-Disposition': f'attachment; filename={filename}'}
-        return Response(file, mimetype='application/octet-stream', headers=headers)  
+@app.route('/download', methods=['GET', 'POST'])
+def download_file():
+    if request.method == 'POST':
+        filename = request.form['filename']
+        directory = app.config['UPLOAD_FOLDER']
+        return send_from_directory(directory, filename, as_attachment=True)
     return render_template('download.html')
   
 @app.route('/upload-form')  
 def upload_form():  
-    return '''  
-        <!doctype html>  
-        <html>  
-           <body>  
-              <h1>Upload file</h1>  
-              <form method="POST" action="/upload" enctype="multipart/form-data">  
-                 <input type="file" name="file">  
-                 <input type="submit" value="Upload">  
-              </form>  
-           </body>  
-        </html>  
-    '''  
+    return render_template('upload.html')
   
 if __name__ == '__main__':  
     app.run(debug=True)
